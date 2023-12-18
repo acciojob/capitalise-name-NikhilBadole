b8fef2025f76cb78a6a718402a24f9ecf0564a08
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-12-18 23:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function upperCase(){
+	let text = document.getElementById("fname");
+	text.value = text.value.toUpperCase();
+}
+document.getElementById("fname").addEventListener("blur", upperCase);
